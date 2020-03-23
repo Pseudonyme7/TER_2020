11d6b37102315091e831034c633dfd43b1f49183
Julien <Aghatorzz@gmail.com>
2020-03-23 13:57:53 +0100

    Suppression de ma partie a cause du manque de place sur le depot

--- user_register_valide(1)/user_register_valide/myproject/myproject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-
-urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^api/', include('myproject.api.urls'))
-]
--- user_register_valide(1)/user_register_valide/web/README.md
+++
@@ -1,4 +0,0 @@
-# Web
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.2.
-
